74fae1b0e0d9eb2ae3e9c48ad4797243eb0fac86
onePoint <124888518+Gvozdkov@users.noreply.github.com>
2023-03-05 11:51:07 +0300

    Initial Commit

---
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+extension UIColor { }
+extension UIColor {
+    static var ypBlack: UIColor { UIColor(named: "ypBlack") ?? UIColor.black }
+    static var ypGreen: UIColor { UIColor(named: "ypGreen") ?? UIColor.green }
+    static var ypRed: UIColor { UIColor(named: "ypRed") ?? UIColor.red }
+    static var yaDarkGray: UIColor { UIColor(named: "yaDarkGray") ?? UIColor.darkGray }
+    static var yaGray: UIColor { UIColor(named: "yaGray") ?? UIColor.gray }
+    static var yaWhite: UIColor { UIColor(named: "yaWhite") ?? UIColor.white }
+}
