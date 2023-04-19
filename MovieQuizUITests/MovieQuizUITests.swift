0544860fa428760e14fb94b9aef7016db124a296
onePoint <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>
2023-04-19 21:49:58 +0300

    Добавлен метод testNoButton в класс MovieQuizUITests, который тестирует кнопку "Нет".

--- MovieQuizUITests/MovieQuizUITests.swift
+++ MovieQuizUITests/MovieQuizUITests.swift
@@ -42,10 +42,22 @@ final class MovieQuizUITests: XCTestCase {
         XCTAssertFalse(firstPosterData == secondPosterData)
     }
     
-    func testIndexLabel() {
+    func testNiBurron() {
+        sleep(3)
+        
+        let firstPoster = app.images["Poster"]
+        let firstPosterData = firstPoster.screenshot().pngRepresentation
+        
+        app.buttons["No"].tap()
+        sleep(3)
+        
+        let secondPoster = app.images["Poster"]
+        let secondPosterData = secondPoster.screenshot().pngRepresentation
+        
         let indexLabel = app.staticTexts["Index"]
         
+        XCTAssertNotEqual(firstPosterData, secondPosterData)
         XCTAssertEqual(indexLabel.label, "2/10")
-        
     }
+    
 }
