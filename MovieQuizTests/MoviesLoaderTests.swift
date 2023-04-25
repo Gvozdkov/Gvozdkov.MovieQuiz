374cc63ee186f6e5fa003f64a819ce8fd9347469
onePoint <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>
2023-04-25 22:17:11 +0300

    1. В MoviesLoaderTests структура StubNetworkClient вынесена выше тестовых функций. 2. Удалены закомментированные  мок-вопросы в QuestionFactory.

--- MovieQuizTests/MoviesLoaderTests.swift
+++ MovieQuizTests/MoviesLoaderTests.swift
@@ -10,29 +10,6 @@ import XCTest
 
 class MoviesLoaderTests: XCTestCase {
     
-    func testSuccessLoading() throws {
-        //Given
-        let stubNetworkClient = StubNetworkClient(emulateError: false) // говорим, что не хотим эмулировать ошибку
-        let loader = MoviesLoader(networkClient: stubNetworkClient)
-        
-        // When
-        let expectation = expectation(description: "Loading expectation")
-        
-        loader.loadMovies { result in
-            // Then
-            switch result {
-            case .success(let movies):
-                // давайте проверим, что пришло, например, два фильма — ведь в тестовых данных их всего два
-                XCTAssertEqual(movies.items.count, 2)
-                expectation.fulfill()
-            case .failure(_):
-                XCTFail("Unexpected failure")
-            }
-        }
-        
-        waitForExpectations(timeout: 1)
-    }
-    
     struct StubNetworkClient: NetworkRouting {
         
         enum TestError: Error { // Тестовая ошибка
@@ -84,7 +61,29 @@ class MoviesLoaderTests: XCTestCase {
         }
     }
     
-
+    func testSuccessLoading() throws {
+        //Given
+        let stubNetworkClient = StubNetworkClient(emulateError: false) // говорим, что не хотим эмулировать ошибку
+        let loader = MoviesLoader(networkClient: stubNetworkClient)
+        
+        // When
+        let expectation = expectation(description: "Loading expectation")
+        
+        loader.loadMovies { result in
+            // Then
+            switch result {
+            case .success(let movies):
+                // давайте проверим, что пришло, например, два фильма — ведь в тестовых данных их всего два
+                XCTAssertEqual(movies.items.count, 2)
+                expectation.fulfill()
+            case .failure(_):
+                XCTFail("Unexpected failure")
+            }
+        }
+        
+        waitForExpectations(timeout: 1)
+    }
+    
     
     func testFailureLoading() throws {
         // Given
